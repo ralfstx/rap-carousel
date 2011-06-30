8f2275a433f482965c2144fb8a6e7455d635a16f
Ralf Sternberg <rsternberg@eclipsesource.com>
2011-06-30 23:45:26 +0200

    Added .gitignore, formatted copyright headers

--- org.eclipse.rap.widgets.carousel/src/org/eclipse/rap/widget/carousel/Carousel.java
+++ org.eclipse.rap.widgets.carousel/src/org/eclipse/rap/widget/carousel/Carousel.java
@@ -1,12 +1,13 @@
-/******************************************************************************* 
-* Copyright (c) 2010 EclipseSource and others. All rights reserved. This
-* program and the accompanying materials are made available under the terms of
-* the Eclipse Public License v1.0 which accompanies this distribution, and is
-* available at http://www.eclipse.org/legal/epl-v10.html
+/*******************************************************************************
+* Copyright (c) 2010, 2011 EclipseSource and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
 *
 * Contributors:
-*   EclipseSource - initial API and implementation
-*******************************************************************************/ 
+*    EclipseSource - initial API and implementation
+*******************************************************************************/
 package org.eclipse.rap.widget.carousel;
 
 import java.util.ArrayList;
@@ -23,7 +24,7 @@ import org.eclipse.swt.widgets.Composite;
 
 
 public class Carousel extends Composite {
-  
+
   private static final String URL = "/carousel/carousel.html";
   private final static String CLICK_FUNCTION = "itemClicked";
   private boolean loaded;
@@ -45,7 +46,7 @@ public class Carousel extends Composite {
         if( wantsToStart ) {
           render();
         }
-      }     
+      }
       public void changed( ProgressEvent event ) {
       }
     } );
@@ -98,7 +99,7 @@ public class Carousel extends Composite {
       }
     }
   }
-  
+
   /**
    * Renders the carousel within the browser when the website
    * was loaded completely.
